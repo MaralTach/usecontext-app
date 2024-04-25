e1313cf8f09c456dd7877a206d39dc0d125b55f7
MaralTach <yoloten.maral@gmail.com>
2024-04-25 20:12:54 +0300

    update

---
+++ src/pages/PrivateRouter.jsx
@@ -0,0 +1,10 @@
+import { Outlet, Navigate } from "react-router-dom"
+import { useContext } from "react"
+import { LoginContext } from "../context/LoginContext"
+
+const PrivateRouter = () => {
+  const {signed} = useContext(LoginContext)
+  return signed ? <Outlet /> : <Navigate to="/login" />
+}
+
+export default PrivateRouter
